d77a31b77f888f83f4456eb4fa2e17f95ef5913b
Yuri Alves Miguel <yuri.snp@hotmail.com>
2020-07-17 22:30:37 -0300

    todo

---
+++ src/pages/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page padding>
+    <ul>
+      <task
+        v-for="{task, index} in tasks"
+        :task="task"
+        :index="index"></task>
+    </ul>
+  </q-page>
+</template>
+
+<script>
+import Task from '../components/Task.vue'
+export default {
+  data () {
+    return {
+      tasks: [
+        {
+          name: 'go to shop',
+          dueDate: '2019/05/02',
+          dueTime: '18:38'
+        },
+        {
+          name: 'get bananas',
+          dueDate: '2019/05/03',
+          dueTime: '14:02'
+        },
+        {
+          name: 'get apples',
+          dueDate: '2019/05/04',
+          dueTime: '16:00'
+        }
+      ]
+    }
+  },
+  methods: {
+    deleteTask (index) {
+      this.tasks.splice(index, 1)
+    }
+  },
+  components: {
+    'task' : Task
+  }
+}
+</script>
+
+<style>
+</style>
